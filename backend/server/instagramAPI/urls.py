7e5010e0840d31dc3aa5d11391693733a635a126
mdaashir <22pt17@psgtech.ac.in>
2024-12-20 23:37:33 +0530

    Refactor Instagram API URLs to use DefaultRouter for cleaner routing and organization

--- backend/server/instagramAPI/urls.py
+++ backend/server/instagramAPI/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
-urlpatterns = [
-    path('get-authorization-code/', views.getAuthorizationCode, name='getAuthorizationCode'),
-    path('get-access-token/', views.getAcessToken, name='getAcessToken'),
-    path('get-user-details/', views.getUserDetails, name='getDetails'),
-    path('get-user-posts/', views.getUserPosts, name='getUserPosts'),
-]
+router = DefaultRouter()
+router.register("get-authorization-code",views.getAuthorizationCode, basename="getAuthorizationCode")
+router.register("get-access-token", views.getAccessToken, basename="getAcessToken")
+router.register("get-user-details", views.getUserDetails, basename="getDetails")
+router.register("get-user-posts", views.getUserPosts, basename="getUserPosts")
+
+urlpatterns = [path("instagram/", include(router.urls))]
